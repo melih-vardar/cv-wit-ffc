65791c8f4c77dfe6968e78c9ec9d2179e269afad
Melih Vardar <mvardar97@gmail.com>
2024-11-06 17:06:07 +0300

    component-name-fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react'
 import './App.css'
 import Footer from './components/Footer'
-import Header from './components/header'
+import Header from './components/Header'
 import Hero from './components/Hero'
 import ModeSwitch from './components/ModeSwitch'
 import Profile from './components/Profile'
