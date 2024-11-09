88d09806deb6841ae1fef3ef284a28be47905f2e
Melih Vardar <mvardar97@gmail.com>
2024-11-09 14:51:15 +0300

    axios-post-and-prefferedLang-added

--- src/context/LangContext.jsx
+++ src/context/LangContext.jsx
@@ -1,12 +1,14 @@
-import { createContext, useEffect } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import langTranslation from '../langTranslation.json'
 import useLocalStorage from '../hooks/useLocalStorage';
+import axios from 'axios';
 
 
 export const LangContext = createContext();
 
 export const LangContextProvider = ({ children }) => {
     const [lang, setLang] = useLocalStorage('lang', 'tr');
+    const [currentContent, setCurrentContent] = useState(null);
 
     const toggleLang = () => {
         if (lang === 'tr') {
@@ -16,11 +18,44 @@ export const LangContextProvider = ({ children }) => {
         }
     }
 
-    const translate = (key) => langTranslation[lang][key] || key;
+
+    useEffect(() => {
+        const userLang = navigator.language || navigator.userLanguage;
+        if (userLang === 'en' || userLang === 'en-US') {
+            setLang('en')
+        }
+        else {
+            setLang('tr')
+        }
+    }, [])
+
+    useEffect(() => {
+        const currentLangData = langTranslation[lang]
+        if (currentLangData) {
+            axios
+                .post('https://reqres.in/api/workintech', currentLangData)
+                .then((res) => {
+                    console.log('data', res.data)
+                    setCurrentContent(res.data)
+                })
+                .catch((error) => {
+                    console.log('error', error);
+                })
+        }
+        else {
+            console.warn('Language data not found for: ', lang)
+        }
+    }, [lang])
+
+    const translate = (key) => {
+        if (currentContent && currentContent[key]) {
+            return currentContent[key];
+        }
+    };
 
     return (
         <LangContext.Provider value={{ toggleLang, translate, lang }}>
-            {children}
+            {currentContent ? children : <div>Loading...</div>}
         </LangContext.Provider>
     );
 };
